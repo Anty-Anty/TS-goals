1eb8e4d05989820d87c2d8b3b04398a30ef82e3b
Anton <ibeliev@gmail.com>
2026-02-18 11:50:34 -0800

    Add InfoBox component and integrate it into App.tsx

--- src/components/CourseGoalList.tsx
+++ src/components/CourseGoalList.tsx
@@ -1,26 +1,52 @@
+import type { ReactNode } from 'react';
+
 import CourseGoal from "./CourseGoal";
 
-import { type CourseGoalType} from "../App"
+import { type CourseGoalType } from "../App"
+
+import InfoBox from "./InfoBox";
 
 interface CourseGoalListProps {
     goals: CourseGoalType[];
-    onDeleteGoal: (id:number) => void;
+    onDeleteGoal: (id: number) => void;
 }
 
 const CourseGoalList = ({ goals, onDeleteGoal }: CourseGoalListProps) => {
+
+    if (goals.length === 0) {
+        return (
+            <InfoBox mode='hint'>
+                You have no course goals yet. Start adding some!
+            </InfoBox>
+        )
+    };
+
+    let warningBox: ReactNode;
+
+    if (goals.length >= 4) {
+        warningBox = (
+            <InfoBox mode="warning">
+                You are collecting a lot of goals. Don't put too much on your plate!
+            </InfoBox>
+        );
+    }
+
     return (
-        <ul>
-            {goals.map((goal) => (
-                <li key={goal.id}>
-                    <CourseGoal
-                        title={goal.title}
-                        description={goal.description}
-                        onDelete = {onDeleteGoal}
-                        id={goal.id}
-                    />
-                </li>
-            ))}
-        </ul>
+        <>
+            {warningBox}
+            <ul>
+                {goals.map((goal) => (
+                    <li key={goal.id}>
+                        <CourseGoal
+                            title={goal.title}
+                            description={goal.description}
+                            onDelete={onDeleteGoal}
+                            id={goal.id}
+                        />
+                    </li>
+                ))}
+            </ul>
+        </>
     )
 };
 
---
+++ src/components/InfoBox.tsx
@@ -0,0 +1,29 @@
+import type { ReactNode } from "react";
+
+type InfoBoxProps = {
+    mode: 'hint' | 'warning',
+    children: ReactNode
+
+}
+
+const InfoBox = ({ mode, children }: InfoBoxProps) => {
+
+    // if mode === 'mode'
+    if (mode === 'hint') {
+        return (
+            <aside className="infobox infobox-hint">
+                <p>{children}</p>
+            </aside>
+        )
+    }
+
+    // if mode === 'warning'
+    return (
+        <aside className="infobox infobox-warning warning--medium">
+            <h2>Warning</h2>
+            <p>{children}</p>
+        </aside>
+    )
+};
+
+export default InfoBox;
